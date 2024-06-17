db4f2abd324d5fc4fbd3b3325144c00607516090
Guyuchen99 <93961613+Guyuchen99@users.noreply.github.com>
2024-06-16 20:20:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+    //Toggle Navbar
+    $(".navbar-icon").click(function () {
+      $(".header .navbar-menu").toggleClass("active");
+      $(".header .navbar-icon").toggleClass("bx-x");
+    });
+  
+    $(".navbar-item").click(function () {
+      $(".header .navbar-menu").removeClass("active");
+      $(".header .navbar-icon").removeClass("bx-x");
+    });
+  
+  
+    // Shop Item Display
+    let shopItems = document.querySelector(".featured .featured-items");
+    shopItems.innerHTML += shopItems.innerHTML;
+  
+    let shopBtn = document.querySelectorAll(".featured .featured-btn-container .featured-btn");
+    let speed = -2;
+  
+    let move = () => {
+      let currentLeft = parseInt(shopItems.style.left) || 0;
+      let newLeft = currentLeft + speed;
+  
+      if (newLeft <= -shopItems.offsetWidth / 2) {
+        newLeft = 0;
+      }
+  
+      if (newLeft > 0) {
+        newLeft -= shopItems.offsetWidth / 2;
+      }
+  
+      shopItems.style.left = newLeft + "px";
+    };
+  
+    let timer = setInterval(move, 30);
+  
+    const stopScroll = () => {
+      clearInterval(timer);
+    };
+  
+    const resumeScroll = () => {
+      timer = setInterval(move, 30);
+    };
+  
+    shopItems.addEventListener("mouseover", stopScroll);
+    shopItems.addEventListener("mouseout", resumeScroll);
+  
+    shopBtn[0].onclick = function () {
+      speed = -2;
+    };
+  
+    shopBtn[1].onclick = function () {
+      speed = 2;
+    };
+  });
+  
